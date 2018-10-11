b7997c5bff3b4dd58e35c8923d67295980b0d57b
Terrence Tejada <tejada.terrence@gmail.com>
2018-10-11 10:16:37 +0800

    Add Auth API connection

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\User;
+
+class AuthController extends Controller
+{
+    /**
+     * user registration API for mobile
+     *
+     * @param Request $request
+     * @return JSON
+     */
+    public function register(Request $request) {
+
+        $this->validate($request, [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        $user->save();
+
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+
+    }
+
+    public function login(Request $request) {
+
+        $this->validate($request, [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(Auth::attempt($credentials)) {
+
+            $user = Auth::user();
+            $tokenResult = $user->createToken('salesforce');
+            $token = $tokenResult->token;
+            $token->save();
+
+            return response()->json([
+                'access_token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer'
+            ]);
+
+        } else {
+
+            return response()->json(['message' => 'Unauthorized'], 401);
+
+        }
+
+    }
+
+     /**
+     * Logout user (Revoke the token)
+     *
+     * @return [string] message
+     */
+    public function logout(Request $request)
+    {
+        Auth::user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+
+}
