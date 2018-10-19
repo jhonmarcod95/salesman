1473ae314f8326c8fbf7a4edfe325439d7a43073
Terrence Tejada <tejada.terrence@gmail.com>
2018-10-19 16:04:20 +0800

    Attendance initial setup

---
+++ database/migrations/2018_10_19_152853_create_attendences_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('schedule_id')->unsigned();
+            $table->timestamp('sign_in')->nullable();
+            $table->string('sign_in_image')->default('attendance/default.jpg');
+            $table->timestamp('sign_out')->nullable();
+            $table->string('sign_out_image')->default('attendance/default.jpg');
+            $table->string('remarks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendences');
+    }
+}
