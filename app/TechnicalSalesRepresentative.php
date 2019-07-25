413cda67a3f10133d4b2ce58555f13add1ea2727
devy.viray <demetrio.viray@lafilgroup.com>
2019-07-25 17:47:31 +0800

    Implement soft delete for tsr

--- app/TechnicalSalesRepresentative.php
+++ app/TechnicalSalesRepresentative.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TechnicalSalesRepresentative extends Model implements Auditable
 {
-    use \OwenIt\Auditing\Auditable;
+    use \OwenIt\Auditing\Auditable,SoftDeletes;
 
     protected $fillable = ['user_id'];
 
