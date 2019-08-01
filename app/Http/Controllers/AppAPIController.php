fac3cf317ba00063230d479bf2438a56c5576f54
tejada.terrence@gmail.com <it.bgc@pik3.local>
2019-08-01 11:52:14 +0800

    Return zero on negative returned budget balance on io

--- app/Http/Controllers/AppAPIController.php
+++ app/Http/Controllers/AppAPIController.php
@@ -147,8 +147,13 @@ class AppAPIController extends Controller
             // Expense Charge Type
             $expenseChargeType = ExpenseChargeType::where('charge_type_id', $internalOrder->chargeType->id);
 
+            // Return value or zero when negative
+            $simulatedBalancedReturn = (double) $toJson[0]['balance_amount'] - $this->getUnprocessSubmittedExpense($expenseChargeType->first()->expenseType->id);
+
+            $zeroOrResult = $simulatedBalancedReturn < 0 ? 0 : $simulatedBalancedReturn;
+
             //Calculate Ramaining total amound
-            $totalBalance = $expenseChargeType->exists() ? abs((double) $toJson[0]['balance_amount'] - $this->getUnprocessSubmittedExpense($expenseChargeType->first()->expenseType->id)) : (double) $toJson[0]['balance_amount'];
+            $totalBalance = $expenseChargeType->exists() ? $zeroOrResult : (double) $toJson[0]['balance_amount'];
 
             $data = array(
                 'id' => $internalOrder->id,
