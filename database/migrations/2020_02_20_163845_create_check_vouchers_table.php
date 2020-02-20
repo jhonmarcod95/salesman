3f77a372aa23d86763aa532bfa0a601133a0673f
jhonmarcod95 <jhonmarcod95@gmail.com>
2020-02-20 16:44:34 +0800

    Init AutoCV

---
+++ database/migrations/2020_02_20_163845_create_check_vouchers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCheckVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('check_vouchers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('doc_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('check_vouchers');
+    }
+}
