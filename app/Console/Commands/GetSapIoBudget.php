4a9a9aa3fea1d7f2051590a14fbf3312df057865
Arjay Lumagdong <arjay.lumagdong@lfuggoc.net>
2020-10-06 11:44:34 +0800

    get sap io budget update

--- app/Console/Commands/GetSapIoBudget.php
+++ app/Console/Commands/GetSapIoBudget.php
@@ -9,6 +9,7 @@ use App\{
     ExpenseSapIoBudget,
     TechnicalSalesRepresentative,
     SalesmanInternalOrder,
+    User,
 };
 
 class GetSapIoBudget extends Command
@@ -45,34 +46,40 @@ class GetSapIoBudget extends Command
     public function handle()
     {
         $pfmc_io_budgets = $this->getPFMCServers();
-        $this->info('Count : ' . $pfmc_io_budgets);
-
-        // $lfug_io_budgets = $this->getLFUGServers();
-        // $this->info('Count : ' . $lfug_io_budgets);
+        $lfug_io_budgets = $this->getLFUGServers();
+        $this->info('PFMC Count : ' . $pfmc_io_budgets . ' LFUG Count : ' . $lfug_io_budgets);
     }
 
     public function getPFMCServers(){
 
+        // $sapConnection = [
+        //     'ashost' => '172.17.1.33',
+        //     'sysnr' => '00',
+        //     'client' => '400',
+        //     'user' => 'payproject',
+        //     'passwd' => 'welcome69'
+        // ];
+
+        //PFMC QAS
         $sapConnection = [
-            'ashost' => '172.17.1.33',
+            'ashost' => '172.17.1.34',
             'sysnr' => '00',
-            'client' => '400',
+            'client' => '778',
             'user' => 'payproject',
-            'passwd' => 'welcome69'
+            'passwd' => 'welcome69+'
         ];
 
         $month = date('m');
-        // $month = '07';
         $year = date('Y');
         
-        //PFMC-FLOUR / PFMC
-        $company = '2';
+        //Companies
+        $company = ['1','2','3','4','5','6','7'];
         $sap_server_pfmc = 'PFMC';
-        $all_tsr = TechnicalSalesRepresentative::with('company')->where('company_id', $company)->get();
+        $all_tsr = User::with('company')->whereIn('company_id', $company)->get();
         
         $count = 0;
         foreach($all_tsr as $k => $tsr){
-            $io_pfmc = SalesmanInternalOrder::where('user_id', $tsr['user_id'])->where('sap_server',$sap_server_pfmc)->get();
+            $io_pfmc = SalesmanInternalOrder::where('user_id', $tsr['id'])->where('sap_server',$sap_server_pfmc)->get();
             $ioBalances = [];
             if($io_pfmc){
                 foreach($io_pfmc as $k => $tsr_io){
@@ -89,18 +96,19 @@ class GetSapIoBudget extends Command
                    
                     if($budget){
                         $expense_tsr_pfmc = [];
-                        $expense_tsr_pfmc['user_id'] = $tsr['user_id'];
-                        $expense_tsr_pfmc['company_id'] = $company;
+                        $expense_tsr_pfmc['user_id'] = $tsr['id'];
+                        $expense_tsr_pfmc['company_id'] = $tsr['company_id'];
                         $expense_tsr_pfmc['io'] = $io;
                         $expense_tsr_pfmc['io_date'] = $year . $month . '01';
                         $expense_tsr_pfmc['budget_balance'] = $budget['GV_OUTPUT'];
                         $expense_tsr_pfmc['planned_budget'] = $budget['GV_T_PLAN_AMT'];
+                        $expense_tsr_pfmc['server'] = $sap_server_pfmc;
 
                         $check_expense_sap_budget = ExpenseSapIoBudget::where('io',$io)->where('io_date',$year . $month . '01')->first();
 
                         if($check_expense_sap_budget){
                             $check_expense_sap_budget->update($expense_tsr_pfmc);
-                            // $count++;
+                            $count++;
                         }else{
                             ExpenseSapIoBudget::create($expense_tsr_pfmc);
                             $count++;
@@ -116,27 +124,35 @@ class GetSapIoBudget extends Command
 
     public function getLFUGServers(){
 
+        // $sapConnection = [
+        //     'ashost' => '172.17.1.37',
+        //     'sysnr' => '00',
+        //     'client' => '100',
+        //     'user' => 'payproject',
+        //     'passwd' => 'welcome69'
+        // ];
+
+        //PFMC QAS
         $sapConnection = [
-            'ashost' => '172.17.1.37',
-            'sysnr' => '00',
-            'client' => '100',
+            'ashost' => '172.17.2.37',
+            'sysnr' => '01',
+            'client' => '778',
             'user' => 'payproject',
-            'passwd' => 'welcome69'
+            'passwd' => 'welcome69+'
         ];
 
         $month = date('m');
-        // $month = '07';
         $year = date('Y');
-
         
-        //PFMC-FLOUR / LFUG
-        $company = '2';
+        //Companies
+        $company = ['1','2','3','4','5','6','7'];
         $sap_server_lfug = 'LFUG';
-        $all_tsr = TechnicalSalesRepresentative::with('company')->where('company_id', $company)->get();
+
+        $all_tsr = User::with('company')->whereIn('company_id', $company)->get();
         
         $count = 0;
         foreach($all_tsr as $k => $tsr){
-            $io_lfug = SalesmanInternalOrder::where('user_id', $tsr['user_id'])->where('sap_server',$sap_server_lfug)->get();
+            $io_lfug = SalesmanInternalOrder::where('user_id', $tsr['id'])->where('sap_server',$sap_server_lfug)->get();
             $ioBalances = [];
             if($io_lfug){
                 foreach($io_lfug as $k => $tsr_io){
@@ -153,18 +169,19 @@ class GetSapIoBudget extends Command
                    
                     if($budget){
                         $expense_tsr_lfug = [];
-                        $expense_tsr_lfug['user_id'] = $tsr['user_id'];
-                        $expense_tsr_lfug['company_id'] = $company;
+                        $expense_tsr_lfug['user_id'] = $tsr['id'];
+                        $expense_tsr_lfug['company_id'] = $tsr['company_id'];
                         $expense_tsr_lfug['io'] = $io;
                         $expense_tsr_lfug['io_date'] = $year . $month . '01';
                         $expense_tsr_lfug['budget_balance'] = $budget['GV_OUTPUT'];
                         $expense_tsr_lfug['planned_budget'] = $budget['GV_T_PLAN_AMT'];
+                        $expense_tsr_lfug['server'] = $sap_server_lfug;
 
                         $check_expense_sap_budget = ExpenseSapIoBudget::where('io',$io)->where('io_date',$year . $month . '01')->first();
 
                         if($check_expense_sap_budget){
                             $check_expense_sap_budget->update($expense_tsr_lfug);
-                            // $count++;
+                            $count++;
                         }else{
                             ExpenseSapIoBudget::create($expense_tsr_lfug);
                             $count++;
