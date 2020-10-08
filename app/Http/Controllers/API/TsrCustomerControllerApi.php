17178ca7f09148d274159453a805519bcd6c9a65
Terrence Tejada <tejada.terrence@gmail.com>
2020-10-08 19:00:11 +0800

    Additional condition for XX customer"

--- app/Http/Controllers/API/TsrCustomerControllerApi.php
+++ app/Http/Controllers/API/TsrCustomerControllerApi.php
@@ -146,6 +146,7 @@ class TsrCustomerControllerApi extends Controller
                 customer_codes.`name` NOT LIKE '%X:%' AND
                 customer_codes.`name` NOT LIKE '%XXX%' AND
                 customer_codes.`name` NOT LIKE '%ONETIME%' AND
+                customer_codes.`name` NOT LIKE '%XX_%' AND
                 tsr_sap_customers.tsr_customer_code = '$tsr_customer_code' AND
                 tsr_sap_customers.customer_code != '$tsr_customer_code'
                 "));
