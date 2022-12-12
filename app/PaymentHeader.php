50837914b6d4c0a731b9f17d12590aae5e5a4c99
Marco <jhonmarcod95@gmail.com>
2022-12-12 14:42:39 +0800

    Consider company code on CV relation;

--- app/PaymentHeader.php
+++ app/PaymentHeader.php
@@ -9,6 +9,7 @@ use function foo\func;
 class PaymentHeader extends Model implements Auditable
 {
 
+    use \Awobaz\Compoships\Compoships;
     use \OwenIt\Auditing\Auditable;
 
     protected $fillable = [
@@ -46,6 +47,6 @@ class PaymentHeader extends Model implements Auditable
     }
 
     public function checkVoucher(){
-        return $this->hasOne(CheckVoucher::class, 'reference_number', 'reference_number');
+        return $this->hasOne(CheckVoucher::class, ['reference_number', 'company_code'], ['reference_number', 'company_code']);
     }
 }
