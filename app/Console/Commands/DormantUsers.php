e833a7582268435a2633ae00891804cef64cfb31
johnreysoler <156890203+johnreysoler@users.noreply.github.com>
2024-04-23 12:47:50 +0800

    Update dromant_account
    1. get the latest date between last login and last sign out

--- app/Console/Commands/DormantUsers.php
+++ app/Console/Commands/DormantUsers.php
@@ -40,30 +40,40 @@ class DormantUsers extends Command
      */
     public function handle()
     {
-        $users = User::with(['roles'=> function($query){
+        $users = User::withTrashed()->with(['roles'=> function($query){
             $query->where('name','Tsr');
         }])
         ->orderBy('id','desc')->get();
-        
-        foreach($users as $user){
-           $validate_attendance = Attendance::where('user_id',$user->id)->orderBy('sign_out','desc')->get()->first();
-           $dormant_days = 0;
-           if($user->last_login_at != null){
-               $dormant_days = now()->diffInDays(Carbon::parse($user->last_login_at));
-           }
-           else{
-               $dormant_days = now()->diffInDays(Carbon::parse($user->created_at));
-               if($user->roles->count() > 0 && $validate_attendance){
-                   $dormant_days = now()->diffInDays(Carbon::parse($validate_attendance->sign_out));
-               }
-           }   
 
-           $user->dormant_days = $dormant_days;
-           if($dormant_days > 90){
-               $user->remarks = 'Dormant User';
-               $user->deleted_at = Carbon::now()->toDateTimeString();
-           }
-           $user->save();
-       }
+        foreach($users as $user){
+            $tsr_attendances = Attendance::where('user_id',$user->id)->orderBy('sign_out','desc')->get()->first();
+            $dormant_days = 0;
+            $last_login_at = $user->last_login_at;
+            $created_at = Carbon::parse($user->created_at)->toDateTimeString();
+            $last_sign_out_at = !empty($tsr_attendances) ? $tsr_attendances->sign_out : null;
+            $deleted_at = null;
+            $remarks = null;
+            $subtrahed = $created_at;
+
+            if(empty($last_login_at)){
+                $subtrahed = !empty($last_sign_out_at) ? $last_sign_out_at : $subtrahed;
+            }
+            else{
+                $subtrahed = !empty($last_sign_out_at) ? $last_login_at > $last_sign_out_at ? $last_login_at : $last_sign_out_at : $last_login_at;
+            }
+
+            $dormant_days = now()->diffInDays(Carbon::parse($subtrahed));
+            
+            if($dormant_days > 90){
+                $remarks = 'Dormant User';
+                $deleted_at = Carbon::now()->toDateTimeString();
+            }
+            
+            $user->dormant_days = $dormant_days;
+            $user->remarks = $remarks;
+            $user->deleted_at = $deleted_at;
+            $user->save();
+            return $user; 
+        }
     }
 }
