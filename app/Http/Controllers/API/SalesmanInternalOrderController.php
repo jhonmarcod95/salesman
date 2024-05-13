bc1a2ee73b2e4bac5348f7abd5a7dbc164621e3c
Jhon Marco <jhonmarcod95@gmail.com>
2024-05-13 16:33:26 +0800

    display IO and SAP server onl,

--- app/Http/Controllers/API/SalesmanInternalOrderController.php
+++ app/Http/Controllers/API/SalesmanInternalOrderController.php
@@ -8,6 +8,6 @@ use App\SalesmanInternalOrder;
 class SalesmanInternalOrderController extends Controller
 {
     public function fetchSalesManInternalOrders($sap_server){
-        return SalesmanInternalOrder::whereHas('user')->where('sap_server', $sap_server)->get();
+        return SalesmanInternalOrder::whereHas('user')->where('sap_server', $sap_server)->get(['internal_order', 'sap_server'])->unique();;
     }
 }
