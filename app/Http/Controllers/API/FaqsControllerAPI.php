9490fd8432c2fdb409eaf675932911b70f5ab87e
Terrence Tejada <terrence.tejada@lfuggoc.net>
2024-09-27 15:00:36 +0800

    Reveret original faq index

--- app/Http/Controllers/API/FaqsControllerAPI.php
+++ app/Http/Controllers/API/FaqsControllerAPI.php
@@ -19,17 +19,18 @@ class FaqsControllerAPI extends Controller
     public function index()
     {
 
-        $externalUrl = 'http://salesforce.lafilgroup.net:8666';  // The URL to open
+        // $externalUrl = 'http://salesforce.lafilgroup.net:8666';  // The URL to open
 
-        // Optionally use the Google Chrome scheme
-        // $externalUrl = 'googlechrome://salesforce.lafilgroup.net:8666';
+        // // Optionally use the Google Chrome scheme
+        // // $externalUrl = 'googlechrome://salesforce.lafilgroup.net:8666';
     
-        // Return a 302 redirect to the external URL
-        return redirect()->away($externalUrl);
+        // // Return a 302 redirect to the external URL
+        // return redirect()->away($externalUrl);
 
         // return redirect()->away('http://salesforce.lafilgroup.net:8666/authority-to-deduct/'.Auth::user()->id);
-        // $faqs = Faq::all();
-        // return FaqResource::collection($faqs);
+        
+        $faqs = Faq::all();
+        return FaqResource::collection($faqs);
     }
 
     /**
