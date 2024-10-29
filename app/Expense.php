831ce724eb3cdf5f950ad5542aadfbb1cf2efea7
Terrence Tejada <terrence.tejada@lfuggoc.net>
2024-10-29 11:13:16 +0800

    Add deleted at in expenses table

--- app/Expense.php
+++ app/Expense.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Expense extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
+
+    use SoftDeletes;
 
     protected $fillable = [
         'amount',
