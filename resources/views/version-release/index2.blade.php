c99b106291125fb5bc252eaaa1f9945aa4c1f4bc
LFUGGOC\stuartjames.yap <stuartjames.yap@lafilgroup.com>
2025-01-14 08:45:47 +0800

    CSS changes for version release tab in login screen

--- resources/views/version-release/index2.blade.php
+++ resources/views/version-release/index2.blade.php
@@ -7,7 +7,9 @@
 
 <div id="app">
 <version-release :authenticated="false" :user-roles="[]"></version-release>
-<a class="h2 p-2 text-primary text-center" href="{{ url('/home') }}">Return to login</a>
+<span class="mt-4 h3 px-2 py-4">
+    <a class="text-primary" href="{{ url('/home') }}"><i class="fas fa-chevron-left mr-2"></i>Return to login</a>
+</span>
 </div>
 
 @include('layouts.script')
