b6b34bb03bf535b49233d5f7ab424bf1bbdf9e16
Devy Viray <demetrio.viray@lfuggoc.net>
2025-09-01 15:47:09 +0800

    Reprocess 4th week of the August

--- app/Console/Commands/PaymentAutoPostingReprocessing.php
+++ app/Console/Commands/PaymentAutoPostingReprocessing.php
@@ -88,7 +88,8 @@ class PaymentAutoPostingReprocessing extends Command
             // '44th_week' => ['2025-07-21','2025-07-27',true],
                 '45th_week' => ['2025-07-28','2025-08-03',true],
                 '46th_week' => ['2025-08-04','2025-08-10',true],
-                '47th_week' => ['2025-08-11','2025-08-17',true]
+                '47th_week' => ['2025-08-11','2025-08-17',true],
+                '48th_week' => ['2025-08-18','2025-08-24',true]
         ];
 
         foreach($back_dates as $back_date){
