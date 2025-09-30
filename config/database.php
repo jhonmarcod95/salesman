334f81334fb1a1a5a8a6cd7d1078d151142f476c
Christian Psalm Dimafelix <christian.dimafelix@lfuggoc.net>
2025-09-30 18:46:34 +0800

    SSO and user tracking integration

--- config/database.php
+++ config/database.php
@@ -39,6 +39,26 @@ return [
             'prefix' => '',
         ],
 
+        'mysql_hr_db' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('HR_DB_HOST', '127.0.0.1'),
+            'port' => env('HR_DB_PORT', '3306'),
+            'database' => env('HR_DB_DATABASE', 'forge'),
+            'username' => env('HR_DB_USERNAME', 'forge'),
+            'password' => env('HR_DB_PASSWORD', ''),
+            'unix_socket' => env('HR_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            // 'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            // ]) : [],
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
