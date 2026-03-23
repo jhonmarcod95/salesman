188482cd455bd7a25180d3817ed140faee137f24
Devy Viray <demetrio.viray@lfuggoc.net>
2026-03-23 19:57:39 +0800

    Reprocessed 3rd week of march 2025

--- app/Console/Commands/PaymentAutoPostingReprocessing.php
+++ app/Console/Commands/PaymentAutoPostingReprocessing.php
@@ -43,7 +43,8 @@ class PaymentAutoPostingReprocessing extends Command
         $sap_server = $this->argument('sap_server');
         $back_dates = [
             '1st_week' => ['2026-03-01','2026-03-01'],
-            '2nd_week' => ['2026-03-02','2026-03-08']
+            '2nd_week' => ['2026-03-02','2026-03-08'],
+            '3rd_week' => ['2026-03-09','2026-03-15']
         ];
 
         foreach($back_dates as $back_date){
